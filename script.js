5ee077e1409b61ebaa6f8a860d0bfbfc549862ca
PlantsOnFire <ashleyaowen@hotmail.com>
2023-06-09 16:42:23 -0400

    rock lyrics & event listeners done, needs functions

---
+++ script.js
@@ -0,0 +1,27 @@
+//Lyrics Database
+let rockBeginnings1 = ["I wanna ", "Let's get ready and", "Tonight is the night to ", "We are in the mood to ", "You and I will ", "In this night we", "Now is the time to", "I will always "];
+let rockMiddles1 = ["rock with you ", "stay this way ", "rock", "love you "];
+let rockEndings1 = ["and party all the time!", "forever!", "until the end of time!", "every night!"];
+let rockBeginnings2 = ["We are the "];
+let rockMiddles2 = ["light! ", "night! ", "best! ", "party! ", "rock! " ];
+let rockEndings2 = ["And we're never gonna stop!", "So let's live it up!", "Shout it out loud!", "Make some noise!", "Stand your ground and shout!", "Let me hear you say it!"];
+let rockBeginnings3 = ["So now we say ", "Don't let me down! Because ", "She was like, ", "Have you heard that ", ""];
+let rockMiddles3 = ["it's gonna be alright ", "the air is filled with romance ", "the night is young and so are we, ", "the rhythm of our bodies is hot, "];
+let rockEndings3 = ["my Darling.", "my love.", "friends.", "everyone."];
+let rockLines = [[rockBeginnings1, rockMiddles1, rockEndings1], [rockBeginnings2, rockMiddles2, rockEndings2], [rockBeginnings3, rockMiddles3, rockEndings3]];
+
+//Generate Lyrics
+let generateLyricsButton = document.getElementById("generateLyrics");
+generateLyricsButton.addEventListener("click", generateLyrics);
+
+function generateLyrics() {
+  let numberOfLines = 
+}
+
+//Clear Song
+let clearSongButton = document.getElementById("clearSong");
+clearSongButton.addEventListener("click", clearSong);
+
+function clearSong() {
+
+}
