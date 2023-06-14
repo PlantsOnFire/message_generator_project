0e268dc4e48780c1301ac988bfcf990b4b05bb2b
Ashley Owen <ashleyaowen@hotmail.com>
2023-06-14 12:03:34 -0400

    adds song genre logic, needs content still

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let rockEndings3 = ["my Darling.", "my love.", "friends.", "everyone."];
 let rockLines = [[rockBeginnings1, rockMiddles1, rockEndings1], [rockBeginnings2, rockMiddles2, rockEndings2], [rockBeginnings3, rockMiddles3, rockEndings3]];
 
 //Titles
-let firstTitle = ["Purple ", "Red ", "Black ", "Dark ", "Silent ", "Metal ", "Heavy ", "Thunder ", "Dust ", "Devil ", "No Rest ", "Evil ", "Party "];
-let secondTitle = ["Rain", "Winds", "Heart", "Rising", "Devil", "Hell", "Motel", "Drive", "Rush"];
+let rockFirstTitle = ["Purple ", "Red ", "Black ", "Dark ", "Silent ", "Metal ", "Heavy ", "Thunder ", "Dust ", "Devil ", "No Rest ", "Evil ", "Party "];
+let rockSecondTitle = ["Rain", "Winds", "Heart", "Rising", "Devil", "Hell", "Motel", "Drive", "Rush"];
 
 //Generate Lyrics
 let generateLyricsButton = document.getElementById("generateLyrics");
@@ -21,18 +21,36 @@ generateLyricsButton.addEventListener("click", generateTitle);
 
 function generateLyrics() {
   let numberOfLines = document.getElementById("numberOfLyrics").value;
+  let genreSelect = document.getElementById("genre").value;
   let completeSong = [];
-  for (let i = 0; i < numberOfLines; i++) {
-    let randomNumber = Math.floor(Math.random() * 3);
-    let beginningOptions = rockLines[randomNumber][0].length;
-    let middleOptions = rockLines[randomNumber][1].length;
-    let endingOptions = rockLines[randomNumber][2].length;
-    let randomBeginning = Math.floor(Math.random() * beginningOptions);
-    let randomMiddle = Math.floor(Math.random() * middleOptions);
-    let randomEnding = Math.floor(Math.random() * endingOptions);
-    completeSong.push(rockLines[randomNumber][0][randomBeginning] + rockLines[randomNumber][1][randomMiddle] + rockLines[randomNumber][2][randomEnding]);
-    console.log(completeSong, randomNumber, randomBeginning, randomMiddle, randomEnding);
-  }
+  switch (genreSelect) {
+    case "rock":
+      for (let i = 0; i < numberOfLines; i++) {
+        let randomNumber = Math.floor(Math.random() * 3);
+        let beginningOptions = rockLines[randomNumber][0].length;
+        let middleOptions = rockLines[randomNumber][1].length;
+        let endingOptions = rockLines[randomNumber][2].length;
+        let randomBeginning = Math.floor(Math.random() * beginningOptions);
+        let randomMiddle = Math.floor(Math.random() * middleOptions);
+        let randomEnding = Math.floor(Math.random() * endingOptions);
+        completeSong.push(rockLines[randomNumber][0][randomBeginning] + rockLines[randomNumber][1][randomMiddle] + rockLines[randomNumber][2][randomEnding]);
+        console.log(completeSong, randomNumber, randomBeginning, randomMiddle, randomEnding);
+      };
+      break;
+    case "love":
+      //insert logic for love songs
+      break;
+    case "jazz":
+        //insert logic for jazz songs
+        break;
+    case "country":
+        //insert logic for country songs
+        break;
+    case "pop":
+      //insert logic for pop songs
+      break;
+  };
+
   let songLength = completeSong.length;
   let formattedSong = "";
   for (let i=0; i < songLength; i++) {
@@ -44,9 +62,30 @@ function generateLyrics() {
 }
 
 function generateTitle() {
-  let random1 = Math.floor(Math.random() * (firstTitle.length -1));
-  let random2 = Math.floor(Math.random() * (secondTitle.length -1));
-  let title = firstTitle[random1] + secondTitle[random2];
+  let genreSelect = document.getElementById("genre").value;
+  let title = '';
+  switch (genreSelect) {
+    case "rock":
+      let random1 = Math.floor(Math.random() * (rockFirstTitle.length -1));
+      let random2 = Math.floor(Math.random() * (rockSecondTitle.length -1));
+      title = rockFirstTitle[random1] + rockSecondTitle[random2];
+      break;
+    case "love":
+      //insert logic for love song title
+      break;
+    case "jazz":
+      //insert logic for jazz song title
+      break;
+    case "country":
+      //insert logic for country song title
+      break;
+    case "pop":
+      //insert logic for country song title
+      break;
+    default:
+      title = "You did not select a genre";
+  }
+
   document.getElementById("songTitle").innerHTML = title;
 }
 
@@ -57,4 +96,5 @@ clearSongButton.addEventListener("click", clearSong);
 function clearSong() {
   document.getElementById("songContent").innerHTML = "";
   document.getElementById("songTitle").innerHTML ="";
+  completeSong = [];
 }
